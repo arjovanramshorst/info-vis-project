b358912b0cd43ab156f7a596087778c4ffd49517
Arjo van Ramshorst <arjovanramshorst@gmail.com>
2019-11-28 12:30:41 +0100

    Updates

---
+++ precache-manifest.a248647499f385ea9af9708933966bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0075c099c38a4149a3884a4c783a96fe",
+    "url": "/info-vis-project/index.html"
+  },
+  {
+    "revision": "52a21843a85b87f18aa6",
+    "url": "/info-vis-project/static/css/2.f7f97c6b.chunk.css"
+  },
+  {
+    "revision": "a2200658f4032c41d3ab",
+    "url": "/info-vis-project/static/css/main.8b246733.chunk.css"
+  },
+  {
+    "revision": "52a21843a85b87f18aa6",
+    "url": "/info-vis-project/static/js/2.926354c3.chunk.js"
+  },
+  {
+    "revision": "a2200658f4032c41d3ab",
+    "url": "/info-vis-project/static/js/main.f54ea217.chunk.js"
+  },
+  {
+    "revision": "5bcd379b0916bae0a723",
+    "url": "/info-vis-project/static/js/runtime-main.34c0296b.js"
+  }
+]);

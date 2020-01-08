887610457269a71c61cc8927eecac3502190c2fc
Arjo van Ramshorst <arjovanramshorst@gmail.com>
2020-01-08 13:58:24 +0100

    Added images to slide 1 and 6

--- src/components/pages/slides/Slide1.tsx
+++ src/components/pages/slides/Slide1.tsx
@@ -2,8 +2,17 @@ import * as React from 'react'
 import { ISlideProps } from './slides.interface'
 import { useEffect } from 'react'
 import { SlideText, SlideTitle } from './slides.components'
+import { url } from '../../../utils/router'
+import styled from 'styled-components'
 
 interface ISlide1 extends ISlideProps {}
+
+const SlideImage = styled.div`
+    background-image: url(${url('/assets/intro_color.png')});
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 400px;
+`
 
 const Slide1: React.FunctionComponent<ISlide1> = ({ setYear, setFeature }) => {
     useEffect(() => {
@@ -14,6 +23,7 @@ const Slide1: React.FunctionComponent<ISlide1> = ({ setYear, setFeature }) => {
     return (
         <div>
             <SlideTitle>How big is the gender gap in the European Union?</SlideTitle>
+            <SlideImage />
             <SlideText>
                 Gender equality is one of the most prominent problems we have yet to solve. In the Sustainable
                 Development Goals initiative, the UN has declared its intention to close the gender gap worldwide by
--- src/components/pages/slides/Slide6.tsx
+++ src/components/pages/slides/Slide6.tsx
@@ -1,17 +1,34 @@
 import * as React from 'react'
 import { SlideText, SlideTitle } from './slides.components'
+import styled from 'styled-components'
+import { url } from '../../../utils/router'
+import { Col, Row } from 'antd'
 
-interface ISlide6 {}
+interface ISlide6 {
+}
+
+const SlideImage = styled.div`
+    background-image: url(${url('/assets/core_domains.png')});
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 600px;
+`
 
 const Slide6: React.FunctionComponent<ISlide6> = ({}) => (
-    <div>
-        <SlideTitle>Core domains.</SlideTitle>
-        <SlideText>
-            In Europe the GEI is measured by 6 core domains, which are relevant to the structure of the european policy.
-            Work, money, knowledge, time, power, and health. The domain with the smallest gap is Health. While the
-            biggest gap is seen in Power.
-        </SlideText>
-    </div>
+    <Row>
+        <Col md={8}>
+            <SlideTitle>Core domains.</SlideTitle>
+            <SlideText>
+                In Europe the GEI is measured by 6 core domains, which are relevant to the structure of the european
+                policy.
+                Work, money, knowledge, time, power, and health. The domain with the smallest gap is Health. While the
+                biggest gap is seen in Power.
+            </SlideText>
+        </Col>
+        <Col md={16}>
+            <SlideImage />
+        </Col>
+    </Row>
 )
 
 export default Slide6
